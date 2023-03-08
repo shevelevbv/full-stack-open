da816a15a9a0ce42b1914d313d57afece26ab967
Bronislav Shevelev <shevelevbv@hotmail.com>
2023-03-07 21:17:58 -0500

    refactor: complete unicafe 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,26 +2,36 @@ import { useState } from "react";
 
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>;
 
-const Statistics = ({text, stats, unit}) => <p>{text} {stats} {unit}</p>;
+const Stat = ({text, data, unit}) => <p>{text} {data} {unit}</p>
 
-const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
+const Statistics = ({good, neutral, bad}) => {
   const GOOD_WEIGHT = 1;
   const NEUTRAL_WEIGHT = 0;
   const BAD_WEIGHT = -1;
 
   const countAll = good + neutral + bad;
+  const average = (good * GOOD_WEIGHT + neutral * NEUTRAL_WEIGHT + bad * BAD_WEIGHT) / countAll;
+  const percentPositive = good / countAll * 100
 
-  const average = countAll
-    ? (good * GOOD_WEIGHT + neutral * NEUTRAL_WEIGHT + bad * BAD_WEIGHT) / countAll
-    : 0;
+  if (!countAll) {
+    return <p>No feedback given</p>
+  }
+  return (
+    <>
+      <Stat text={'good'} data={good}/>
+      <Stat text={'neutral'} data={neutral}/>
+      <Stat text={'bad'} data={bad}/>
+      <Stat text={'all'} data={countAll}/>
+      <Stat text={'average'} data={average}/>
+      <Stat text={'positive'} data={percentPositive} unit={'%'}/>
+    </>
+  )
+};
 
-  const percentPositive = countAll
-    ? good / countAll * 100
-    : 0;
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
@@ -30,12 +40,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text={'neutral'}/>
       <Button handleClick={() => setBad(bad + 1)} text={'bad'}/>
       <h2>statistics</h2>
-      <Statistics text={'good'} stats={good} />
-      <Statistics text={'neutral'} stats={neutral} />
-      <Statistics text={'bad'} stats={bad} />
-      <Statistics text={'all'} stats={countAll} />
-      <Statistics text={'average'} stats={average} />
-      <Statistics text={'positive'} stats={percentPositive} unit={'%'}/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
