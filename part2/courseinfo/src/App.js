0784c123786882c4291fd9cb25643a212a2ffde7
Bronislav Shevelev <shevelevbv@hotmail.com>
2023-03-09 18:31:38 -0500

    feat: complete courseinfo 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,27 +1,4 @@
-const Header = ({ course }) => <h3>{course}</h3>;
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((init, x) => init + x.exercises, 0);
-  return <h4>total of {total} exercises</h4>;
-};
-
-const Part = ({ part }) =>
-  <p>
-    {part.name} {part.exercises}
-  </p>;
-
-const Content = ({ parts }) =>
-  <>
-    {parts.map(part => <Part key={part.id} part={part} />)}
-  </>;
-
-const Course = ({course}) => (
-  <>
-    <Header course={course.name} />
-    <Content parts={course.parts}/>
-    <Total parts={course.parts}/>
-  </>
-);
+import Course from "./Course";
 
 const App = () => {
   const title = 'Web development curriculum';
